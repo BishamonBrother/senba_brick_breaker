95b666cc706ded324553aaf7cd72dcac1b59384c
bishamon_brother <bishamon.brother@gmail.com>
2022-10-19 14:50:28 +0900

    :sparkles:パドルの描画と移動操作処理を追加

--- src/components/BrickBreaker.vue
+++ src/components/BrickBreaker.vue
@@ -35,6 +35,11 @@ export default {
         new Audio(require('@/assets/sounds/chi.wav')),
         new Audio(require('@/assets/sounds/kan.wav')),
       ],
+      paddleHeight: 10,
+      paddleWidth: 75,
+      paddleX: 0,
+      rightPressed: false,
+      leftPressed: false
     }
   },
 
@@ -43,41 +48,82 @@ export default {
     this.x = canvas.width / 2;
     this.y = canvas.height - 30;
     setInterval(this.draw, 10);
-    this.se.forEach(se => se.volume = 0.0)
+    this.se.forEach(se => se.volume = 0.00);
+    this.paddleX = (canvas.width - this.paddleWidth) / 2;
+    document.addEventListener('keydown', this.onKeyDown);
+    document.addEventListener('keyup', this.onKeyUp);
   },
+
+  computed: {
+    canvas () {
+      return this.$refs["brickBreakerCanvas"];
+    },
+
+    ctx () {
+      return this.canvas.getContext("2d");
+    }
+  },
+
   methods: {
     drawBall: function () {
-      const canvas = this.$refs["brickBreakerCanvas"];
-      const ctx = canvas.getContext("2d");
-      
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      this.ctx.beginPath();
+      this.ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
+      this.ctx.fillStyle = "#0095DD";
+      this.ctx.fill();
+      this.ctx.closePath();
     },
-    draw: function () {
-      const canvas = this.$refs["brickBreakerCanvas"];
-      const ctx = canvas.getContext("2d");
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    draw: function () {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.drawBall();
+      this.drawPaddle();
 
       this.x += this.dx;
       this.y += this.dy;
 
-      if (this.x + this.dx > canvas.width - this.ballRadius || this.x + this.dx < this.ballRadius) {
+      if (this.x + this.dx > this.canvas.width - this.ballRadius || this.x + this.dx < this.ballRadius) {
         this.dx = -this.dx;
         this.se[this.getRandomInterger(this.se.length)].play();
       }
 
-      if (this.y + this.dy > canvas.height - this.ballRadius || this.y + this.dy < this.ballRadius) {
+      if (this.y + this.dy > this.canvas.height - this.ballRadius || this.y + this.dy < this.ballRadius) {
         this.dy = -this.dy;
         this.se[this.getRandomInterger(this.se.length)].play();
       }
+
+      if (this.rightPressed) {
+        this.paddleX = Math.min(this.paddleX + 3, this.canvas.width - this.paddleWidth);
+      } else if (this.leftPressed) {
+        this.paddleX = Math.max(this.paddleX - 3, 0);
+      }
     },
+
     getRandomInterger: function (max) {
       return Math.floor(Math.random() * max);
+    },
+
+    drawPaddle: function () {
+      this.ctx.beginPath();
+      this.ctx.rect(this.paddleX, this.canvas.height - this.paddleHeight, this.paddleWidth, this.paddleHeight);
+      this.ctx.fillStyle = "#0095DD";
+      this.ctx.fill();
+      this.ctx.closePath();
+    },
+
+    onKeyDown: function (e) {
+      if (e.key === "Right" || e.key === "ArrowRight") {
+        this.rightPressed = true;
+      } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        this.leftPressed = true;
+      }
+    },
+
+    onKeyUp: function (e) {
+      if (e.key === "Right" || e.key === "ArrowRight") {
+        this.rightPressed = false;
+      } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        this.leftPressed = false;
+      }
     },
   }
 }
