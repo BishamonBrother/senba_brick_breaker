83d1c33ec5588b4355b377b00ed1c0277168e942
bishamon_brother <bishamon.brother@gmail.com>
2022-10-19 22:42:29 +0900

    :sparkles:クリア時の処理を作成

--- src/components/BrickBreaker.vue
+++ src/components/BrickBreaker.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="brickBreaker" align="center">
-    <canvas ref="brickBreakerCanvas" width="960" height="540">
+    <canvas ref="brickBreakerCanvas" width="940" height="540">
     </canvas>
   </div>
 </template>
@@ -16,15 +16,22 @@ export default {
     return {
       x: 0,
       y: 0,
-      dx: 3,
+      dx: 2,
       dy: 3,
       ballRadius: 10,
-      se: [
+      breakSe: [
         new Audio(require('@/assets/sounds/pon.wav')),
         new Audio(require('@/assets/sounds/chi.wav')),
         new Audio(require('@/assets/sounds/kan.wav')),
       ],
       gameOverSe: new Audio(require('@/assets/sounds/SE_YAKU.wav')),
+      ikkitsuukanSe: new Audio(require('@/assets/sounds/SE_DORA.wav')),
+      reachSe: new Audio(require('@/assets/sounds/reach.wav')),
+      clearSe: [
+        new Audio(require('@/assets/sounds/SE_AGARI.wav')),
+        new Audio(require('@/assets/sounds/ron.wav')),
+        new Audio(require('@/assets/sounds/tsumo.wav')),
+      ],
       paddleHeight: 10,
       paddleWidth: 100,
       paddleX: 0,
@@ -34,12 +41,15 @@ export default {
       brickRowCount: 10,
       brickColumnCount: 11,
       brickWidth: 80,
-      brickHeight: 20,
-      brickPadding: 1,
+      brickHeight: 30,
+      brickPadding: 0.1,
       brickOffsetTop: 30,
       brickOffsetLeft: 30,
       bricks: [],
       bgImage: new Image(),
+      ikkitsuukan: false,
+      isReach: false,
+      isGameCleared: false,
     }
   },
 
@@ -47,8 +57,16 @@ export default {
     this.x = this.canvas.width / 2;
     this.y = this.canvas.height - 30;
     this.interval = setInterval(this.draw, 10);
-    this.se.forEach(se => se.volume = 0.1);
+    this.breakSe.forEach(se => se.volume = 0.1);
     this.gameOverSe.volume = 0.1;
+    this.ikkitsuukanSe.volume = 0.1;
+    this.reachSe.volume = 0.1;
+    this.clearSe.forEach(se => se.volume = 0.1);
+    this.clearSe.forEach(se => se.onended = function () {
+      alert("お見事なのじゃ！");
+      document.location.reload();
+      clearInterval(this.interval);
+    });
     this.paddleX = (this.canvas.width - this.paddleWidth) / 2;
     document.addEventListener('keydown', this.onKeyDown);
     document.addEventListener('keyup', this.onKeyUp);
@@ -60,16 +78,7 @@ export default {
       }
     }
 
-    // const backgroundImage = new Image();
-    // backgroundImage.src = require('@/assets/images/background.jpg');
-    // this.bgImage.onload = ()=>{
-    //   this.ctx.drawImage(this.bgImage, 0, 0);
-    // }
-    // this.bgImage.src = require('@/assets/images/background.jpg');
-    // // this.ctx.drawImage(this.bgImage, 0, 0);
-    // this.bgImage.onload = ()=>{
-    //   this.ctx.drawImage(this.bgImage, 0, 0);
-    // }
+    this.bgImage.src = require('@/assets/images/background.jpg');
   },
 
   computed: {
@@ -93,6 +102,7 @@ export default {
 
     draw: function () {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.drawBgImage();
       this.drawBall();
       this.drawPaddle();
       this.drawBricks();
@@ -104,7 +114,6 @@ export default {
       /* 左右の壁で跳ね返る処理 */
       if (this.x + this.dx > this.canvas.width - this.ballRadius || this.x + this.dx < this.ballRadius) {
         this.dx = -this.dx;
-        // this.se[this.getRandomInterger(this.se.length)].play();
       }
 
       /* 上の壁で跳ね返る処理 */
@@ -113,20 +122,30 @@ export default {
       } else if (this.y + this.dy > this.canvas.height - this.ballRadius) {
         if (this.x > this.paddleX && this.x < this.paddleX + this.paddleWidth) {
           /* パドルで跳ね返る処理
-             中心付近（1/4~3/4）に当たると横軸の移動が緩くなる。 */
-          if (this.x > this.paddleX + this.paddleWidth / 4 && this.x < this.paddleX + this.paddleWidth * 3 / 4) {
-            this.dy = -this.dy;
+             中心付近に当たると横軸の移動が緩くなる。
+             さらに中心付近に当たると一気通貫モードになる。 */
+          if (this.x > this.paddleX + this.paddleWidth * 8 / 17 && this.x < this.paddleX + this.paddleWidth * 9 / 17) {
+            this.dy = this.dy > 0 ? -5 : 5;
             this.dx = this.dx > 0 ? 1 : -1;
+            this.ikkitsuukan = true;
+            this.ikkitsuukanSe.play();
+          } else if (this.x > this.paddleX + this.paddleWidth / 4 && this.x < this.paddleX + this.paddleWidth * 3 / 4) {
+            this.dy = this.dy > 0 ? -3 : 3;
+            this.dx = this.dx > 0 ? 1 : -1;
+            this.ikkitsuukan = false;
           } else {
-            this.dy = -this.dy;
-            this.dx = this.dx > 0 ? 3 : -3;
+            this.dy = this.dy > 0 ? -3 : 3;
+            this.dx = this.dx > 0 ? 2 : -2;
+            this.ikkitsuukan = false;
           }
         } else {
           /* 下に触れるとゲームオーバーになる処理 */
-          this.gameOverSe.play();
-          alert("ゲームオーバーなのじゃ！");
-          document.location.reload();
-          clearInterval(this.interval);
+          if (!this.isGameCleared) {
+            this.gameOverSe.play();
+            alert("ゲームオーバーなのじゃ！");
+            document.location.reload();
+            clearInterval(this.interval);
+          }
         }
       }
 
@@ -159,12 +178,10 @@ export default {
             this.bricks[c][r].y = brickY;
             this.ctx.beginPath();
             this.ctx.rect(brickX, brickY, this.brickWidth, this.brickHeight);
-            // this.ctx.fillStyle = "#0095DD";
   
             let red = 255 - 30 * c;
             let green = 255 - 30 * r;
             this.ctx.fillStyle = 'rgb(' + Math.floor(red) + ', ' + Math.floor(green) + ', 0)';
-            // this.ctx.strokeStyle = "black";
             this.ctx.fill();
             this.ctx.closePath();
           }
@@ -172,18 +189,45 @@ export default {
       }
     },
 
+    drawBgImage: function () {
+      this.ctx.drawImage(this.bgImage, 30, 30, 880, 293);
+    },
+
     collisionDetection: function() {
+      let isAllDeleted = true;
+      let numBricks = 0;
       for (let c = 0; c < this.brickColumnCount; c++) {
         for (let r = 0; r < this.brickRowCount; r++) {
           const b = this.bricks[c][r];
           if (b.exists) {
-            if (this.x > b.x && this.x < b.x + this.brickWidth && this.y > b.y && this.y < b.y + this.brickHeight) {
-              this.se[this.getRandomInterger(this.se.length)].play();
-              this.dy = -this.dy;
-              b.exists = false;
+            numBricks++;
+            isAllDeleted = false;
+            if (this.ikkitsuukan) {
+              if (this.x > b.x && this.x < b.x + this.brickWidth && this.y > b.y && this.y < b.y + this.brickHeight) {
+                b.exists = false;
+              }
+            } else {
+              if (this.x > b.x && this.x < b.x + this.brickWidth && this.y > b.y && this.y < b.y + this.brickHeight) {
+                if (!this.isReach) {
+                  this.breakSe[this.getRandomInterger(this.breakSe.length)].play();
+                }
+                this.dy = -this.dy;
+                b.exists = false;
+              }
             }
           }
         }
+      }
+
+      if (numBricks === 1 && !this.isReach) {
+        this.isReach = true;
+        this.reachSe.play();
+      }
+
+      /* クリア時の処理 */
+      if (isAllDeleted && !this.isGameCleared) {
+        this.isGameCleared = true;
+        this.clearSe[this.getRandomInterger(this.clearSe.length)].play();
       }
     },
 
